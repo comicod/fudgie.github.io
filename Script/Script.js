4e37ce568f2404328f8d0a571655339f00fd05fa
Comicod stodio <107433160+comicod@users.noreply.github.com>
2024-01-16 20:57:22 +0330

    Add files via upload

---
+++ Script/Script.js
@@ -0,0 +1,7 @@
+function OburgerMenu() {
+    document.getElementById("Menu-b").className = "burger-menuOpen";
+}
+
+function CburgerMenu() {
+    document.getElementById("Menu-b").className = "burger-menuClose";
+}
